1c9796879ec9486f9f211f933ac070c061d2f25d
Nikhil <nikhil.p@hotmail.co.uk>
2020-08-15 21:26:19 +0100

    Reformatted file

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from operator import itemgetter
+
 res = open('lotteryData/649.csv', 'r', encoding='utf-8-sig')
 splitResults = res.read().split('\n')
 splitResults.pop()
 for i in range(len(splitResults)):
     splitResults[i] = splitResults[i].split(',')
 
-nums = dict([(i, 0) for i in range(0,50)])
+nums = dict([(i, 0) for i in range(0, 50)])
 
 rows = len(splitResults)
 cols = len(splitResults[0])
 
-
-for i in range(1,rows):
-    for j in range(4,cols):
+for i in range(1, rows):
+    for j in range(4, cols):
         stringNum = splitResults[i][j]
         if stringNum != '':
             drawnNum = int(splitResults[i][j])
             nums[drawnNum] += 1
 
-filledNums = [ (k,v) for k, v in nums.items() ]
-print(filledNums)
-filledNums.sort(key = lambda x:x[1])
+filledNums = [(k, v) for k, v in nums.items()]
+filledNums.sort(key=lambda x: x[1])
 
 for i in range(7):
     print("Number: %d, Count: %d" % filledNums.pop())
